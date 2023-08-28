f4fae5ff74a23e9ff1cdeba3111fe7431e211561
Fabio A. Gomez Diaz <Fabio.Diaz@daugherty.com>
2023-08-28 14:49:01 -0500

    - Adding /helloworld route
    Signed-off-by: Fabio A. Gomez Diaz <Fabio.Diaz@daugherty.com>

--- src/main/java/com/demo/springbootmicroservice/SpringbootMicroserviceApplication.java
+++ src/main/java/com/demo/springbootmicroservice/SpringbootMicroserviceApplication.java
@@ -2,12 +2,22 @@ package com.demo.springbootmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SpringbootMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMicroserviceApplication.class, args);
+    }
+}
 
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String helloWorld() {
+        return "Hello, World!";
+    }
 }
