dab1cdcfcfd11f4aebf709ceb02d2c6ac2fcb93f
Fabio A. Gomez Diaz <Fabio.Diaz@daugherty.com>
2023-09-11 12:55:58 -0500

    KT-20: Print the environment type, hostname, and IP address
    Signed-off-by: Fabio A. Gomez Diaz <Fabio.Diaz@daugherty.com>

--- src/main/java/com/demo/springbootmicroservice/SpringbootMicroserviceApplication.java
+++ src/main/java/com/demo/springbootmicroservice/SpringbootMicroserviceApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @SpringBootApplication
 public class SpringbootMicroserviceApplication {
 
@@ -18,6 +22,32 @@ class HelloWorldController {
 
     @GetMapping("/helloworld")
     public String helloWorld() {
-        return "Hello, World!";
+        String hostname = System.getenv("HOSTNAME");
+        String ipAddress = "Unknown";
+        String environment = "unknown";
+
+        try {
+            ipAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        // Check if running in Docker
+        File dockerFile = new File("/.dockerenv");
+        if (dockerFile.exists()) {
+            environment = "Docker";
+        }
+
+        // Check if running in Kubernetes
+        if (System.getenv("KUBERNETES_PORT") != null) {
+            environment = "Kubernetes";
+        }
+
+        return String.format("<html><body>" +
+                             "Hello, World!<br>" +
+                             "Running in: %s<br>" +
+                             "From Host: %s<br>" +
+                             "IP Address: %s" +
+                             "</body></html>", environment, hostname, ipAddress);
     }
 }
